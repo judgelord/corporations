b821b50c1c4cc646fa9388d16478bf53826ffb9a
judgelord <devin.jl@gmail.com>
2026-02-09 12:12:35 -0500

    #2

--- sandbox/make_example_data.R
+++ sandbox/make_example_data.R
@@ -4,5 +4,5 @@ html <- read_html("https://www.ft.com/companies") # The UN homepage
 links <- html_nodes(html, "a") # "a" nodes are linked text
 html_text(links)
 
-ft <- html_text(links)[20:320]
+ft <- html_text(links)[27:326]
 save(ft, file = here::here("data", "ft.rd"))
