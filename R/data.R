1f0c79bea45f81a3c92ce9b329d33fab488296db
stevenhanwen <stevenhanwen.kang@outlook.com>
2026-02-10 02:23:06 -0500

    updated gitignore file

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' corporations_data dataset
+#'
+#' A crosswalk lookup table of hundreds of thousands of corporations for regex matching and company searching
+#' TODO: ONCE FINAL CROSSWALK IS DESIGNED, ADD DOCUMENTATION FOR THIS DATA
+#' @format A tibble with ____ columns:
+#' \describe{
+#'   \item{column_1}{Description of column 1}
+#'   \item{column_2}{Description of column 2}
+#' }
+#' @source \url{http://}
+"corporations_data"
